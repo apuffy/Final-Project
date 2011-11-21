aebc0c39196e6d1c5846c5143dadc50804234829
Andrew Adams <apuffy87@gmail.coms>
2011-11-20 19:35:25 -0600

    partial

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -6,12 +6,12 @@ describe SessionsController do
   describe "GET 'login'" do
 
     it "should be successful" do
-      get 'login'
+      get :login
       response.should be_success
     end
 
     it "should have the right title" do
-      get 'login'
+      get :login
       response.should have_selector("title", :content => "Sign In")
     end
   end
