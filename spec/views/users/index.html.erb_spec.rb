e6f2e44e409a19a24baa3c8a745b9261e46a990e
Andrew Adams <apuffy87@gmail.com>
2011-11-22 18:09:25 -0600

    scaffold edit

---
+++ spec/views/users/index.html.erb_spec.rb
@@ -0,0 +1,44 @@
+require 'spec_helper'
+
+describe "users/index.html.erb" do
+  before(:each) do
+    assign(:users, [
+      stub_model(User,
+        :name => "Name",
+        :email => "Email",
+        :phone => 1,
+        :address => "Address",
+        :city => "City",
+        :state => "State",
+        :password => "Password"
+      ),
+      stub_model(User,
+        :name => "Name",
+        :email => "Email",
+        :phone => 1,
+        :address => "Address",
+        :city => "City",
+        :state => "State",
+        :password => "Password"
+      )
+    ])
+  end
+
+  it "renders a list of users" do
+    render
+    # Run the generator again with the --webrat flag if you want to use webrat matchers
+    assert_select "tr>td", :text => "Name".to_s, :count => 2
+    # Run the generator again with the --webrat flag if you want to use webrat matchers
+    assert_select "tr>td", :text => "Email".to_s, :count => 2
+    # Run the generator again with the --webrat flag if you want to use webrat matchers
+    assert_select "tr>td", :text => 1.to_s, :count => 2
+    # Run the generator again with the --webrat flag if you want to use webrat matchers
+    assert_select "tr>td", :text => "Address".to_s, :count => 2
+    # Run the generator again with the --webrat flag if you want to use webrat matchers
+    assert_select "tr>td", :text => "City".to_s, :count => 2
+    # Run the generator again with the --webrat flag if you want to use webrat matchers
+    assert_select "tr>td", :text => "State".to_s, :count => 2
+    # Run the generator again with the --webrat flag if you want to use webrat matchers
+    assert_select "tr>td", :text => "Password".to_s, :count => 2
+  end
+end
