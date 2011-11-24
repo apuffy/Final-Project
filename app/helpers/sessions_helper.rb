67767ac6d24291861b9793cb96c41e6e8ee6d641
Andrew Adams <apuffy87@gmail.com>
2011-11-24 13:20:56 -0600

    no luck signin

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,6 +1,19 @@
 module SessionsHelper
   def sign_in(user)
-    cookies.permanent.signed[:remember_token] = [user.id, user.salt]
+    cookies.permanent.signed[:remember_token] = [user.id]
     self.current_user = user
+  end
+  
+  def current_user=(user)
+    @current_user = user
+  end
+  
+  def signed_in?
+    !current_user.nil?
+  end
+  
+  def sign_out
+    cookies.delete(:remember_token)
+    self.current_user = nil
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,13 @@
 class User < ActiveRecord::Base
   attr_accessor :password
-  attr_accessible :name, :email, :password, :password_confirmation
+  attr_accessible :name, :email, :password
 
   validates :password, :presence     => true,
                        :confirmation => true,
                        :length       => { :within => 6..40 }
                        
   def has_password?(submitted_password)
-    user.password == (submitted_password)
+    user:password == (submitted_password)
   end
   
   def self.authenticate(email, submitted_password)
