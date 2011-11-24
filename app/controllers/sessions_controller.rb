4f3f0b7c338195481c28cb3d919c859d50ed893c
Andrew Adams <apuffy87@gmail.com>
2011-11-24 13:56:16 -0600

    stuggling

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,6 +4,7 @@ class SessionsController < ApplicationController
   end
   
   def create
+    flash.now[:notice] = "Got here!!!"
     user = User.authenticate(params[:session][:email],
                              params[:session][:password])
     if user.nil?
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ FinalProject::Application.routes.draw do
   resources :users
   resources :sessions, :only => [:new, :create, :destory]
   
-  root :to => "sessions#login"
+  root :to => "sessions#new"
   match 'signup', :to => "users#new"
   match '/signin',  :to => 'sessions#new'
   match '/signout', :to => 'sessions#destroy'
