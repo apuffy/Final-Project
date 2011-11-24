3ca901f6f3c7ffd201dea27e95e119a795bf3659
Andrew Adams <apuffy87@gmail.com>
2011-11-24 12:47:28 -0600

    signin stuff

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ActiveRecord::Base
+  attr_accessor :password
+  attr_accessible :name, :email, :password, :password_confirmation
+
+  validates :password, :presence     => true,
+                       :confirmation => true,
+                       :length       => { :within => 6..40 }
 end
