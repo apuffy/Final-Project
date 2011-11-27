9427efc7bc64b0d19d1368f4d754f8ba0aea74d3
Andrew Adams <apuffy87@gmail.com>
2011-11-27 16:01:41 -0600

    path correcting

--- config/routes.rb
+++ config/routes.rb
@@ -5,10 +5,10 @@ FinalProject::Application.routes.draw do
   
   root :to => "sessions#new"
   match 'signup', :to => "users#new"
-  match '/signin',  :to => 'sessions#new'
+  match '/signin', :to => 'sessions#new'
   match '/signout', :to => 'sessions#destroy'
   match 'addcontact' , :to => 'pages#addcontact'
-  match 'profile' , :to => 'pages#profile'
+ #match 'profile' , :to => 'pages#profile'
   match 'message', :to => 'pages#message'
   match 'search', :to => "pages#search"
   # The priority is based upon order of creation:
