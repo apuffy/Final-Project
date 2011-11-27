94119be4e24198558d6b3d4c5b5cae579b113e45
Andrew Adams <apuffy87@gmail.com>
2011-11-27 15:30:11 -0600

    temp solution for login in problem

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,21 +1,19 @@
 class User < ActiveRecord::Base
-  attr_accessor :email, :password
-  attr_accessible :name, :email, :phone, :address, :city, :state, :password
-
+  attr_accessor :password
+  attr_accessible :name, :email, :phone, :address, :city, :state,:password
   validates :password, :presence     => true,
                        :confirmation => true,
                        :length       => { :within => 6..40 }
                        
   def has_password?(submitted_password)
-    @password == (submitted_password)
+    :password == (submitted_password)
   end
   
-  def self.authenticate(email, password)
-    if user = User.where(:email => "apuffy87@gmail.com")
-      if @user.password == "tiburon"
-        return user
-      end
-    end
-    return nil
-   end
+  def self.authenticate(email, password) 
+    user = find_by_email(email)
+   #if(user.password == password)
+      return user unless user.nil?
+   #end
+  end
+  
 end
