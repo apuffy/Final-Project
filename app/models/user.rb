fca7d4226cb747df86af9658ee9ecb446e55e9c1
Andrew Adams <apuffy87@gmail.com>
2011-11-28 15:52:15 -0600

    finalizing for lab08

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,10 @@ class User < ActiveRecord::Base
   validates :name, :presence => true, :length => {:within => 3..40}
   validates_format_of :email, :presence => true, 
                       :with => /^([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})$/i
+  validates :address, :presence => true, :length => {:within => 1..40}
+  validates :city, :presence => true, :length => {:within => 1..40}
+  validates :state, :presence => true, :length => {:within => 1..40}
+  validates :phone, :presence => true, :length => {:within => 1..40}
                        
   def has_password?(submitted_password)
     :password == (submitted_password)
