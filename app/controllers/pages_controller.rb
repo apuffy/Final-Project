9c2b9fef0f8a436ee595ee0837bda50c7652b1b1
Andrew Adams <apuffy87@gmail.com>
2011-12-09 13:46:22 -0600

    Session got and pages updated

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,11 @@
 class PagesController < ApplicationController
   def addcontact
    @title = "Add Contact"
+   @user = User.new
+   respond_to do |format|
+   format.html # new.html.erb
+   format.json { render json: @user }
+   end
   end
 
   def message
@@ -9,9 +14,14 @@ class PagesController < ApplicationController
 
   def profile
     @title = "My Profile"
-    @user = User.where(:id => cookies[:remember_token])
-  end
+    @user = User.find_by_id(cookies[:remember_token])
 
+    respond_to do |format|
+      format.html # show.html.erb
+      format.json { render json: @user }
+    end
+  end
+  
   def search
     @title = "Search"
   end
