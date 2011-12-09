b709060db0524cefd69ca6398ac15da24c2feb4a
Andrew Adams <apuffy87@gmail.com>
2011-12-09 12:56:24 -0600

    first db pull

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -8,6 +8,14 @@ module SessionsHelper
     @current_user = user
   end
   
+  def current_user?(user)
+    user == current_user
+  end
+  
+  def current_user
+    @current_user ||= user_from_remember_token
+  end
+  
   def signed_in?
     !current_user.nil?
   end
@@ -15,5 +23,15 @@ module SessionsHelper
   def sign_out
     cookies.delete(:remember_token)
     self.current_user = nil
+  end
+  
+  private
+  def user_from_remember_token
+    userEmail = remember_token
+    User.find_by_email(userEmail)
+  end
+
+  def remember_token
+    cookies[:remember_token] || nil
   end
 end
