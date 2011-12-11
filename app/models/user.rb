312e7458e117ddf1adf96b649c630e663f6cfd8c
Andrew Adams <apuffy87@gmail.com>
2011-12-11 15:33:56 -0600

    fix problem

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,21 +1,21 @@
 class User < ActiveRecord::Base
   attr_accessible :name, :email, :phone, :address, :city, :state,:password
   
-  validates :password, :presence     => true,
-                       :length       => { :within => 5..40 }          
+  validates :password, :presence => true,
+                       :length => { :within => 5..40 }
   validates :name, :presence => true, :length => {:within => 3..40}
-  validates_format_of :email, :presence => true, 
+  validates_format_of :email, :presence => true,
                       :with => /^([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})$/i
   validates :address, :presence => true, :length => {:within => 1..40}
   validates :city, :presence => true, :length => {:within => 1..40}
   validates :state, :presence => true, :length => {:within => 1..40}
-  validates :phone, :presence => true, :length => {:within => 10.10}
+  validates :phone, :presence => true, :length => {:within => 10..10}
                        
   def has_password?(submitted_password)
     password == (submitted_password)
   end
   
-  def self.authenticate(email, password) 
+  def self.authenticate(email, password)
     user = find_by_email(email)
     return nil if user.nil?
     return user if user.has_password?(password)
